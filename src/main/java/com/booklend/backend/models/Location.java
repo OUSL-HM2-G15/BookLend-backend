3273ee0ebdb2f425df83e3f30a5f39b61f8c22d8
zainab fathima <zainab9j3@gmail.com>
2025-12-03 21:38:01 +0530

    Feat: Add API endpoint to fetch all locations

---
+++ src/main/java/com/booklend/backend/models/Location.java
@@ -0,0 +1,34 @@
+package com.booklend.backend.models;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "locations")
+public class Location {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "location_id")
+    private Long locationId;
+
+    @Column(name = "location_name", nullable = false)
+    private String locationName;
+
+    // getters and setters
+
+    public Long getLocationId() {
+        return locationId;
+    }
+
+    public void setLocationName(Long locationId) {
+        this.locationId = locationId;
+    }
+
+    public String getLocationName() {
+        return locationName;
+    }
+
+    public void setLocationName(String locationName) {
+        this.locationName = locationName;
+    }
+}
