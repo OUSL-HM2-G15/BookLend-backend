59d400359eedf5d4c2f84f90d164e7a8e91d78a3
zainab fathima <zainab9j3@gmail.com>
2025-12-17 15:56:04 +0530

    feat: add backend API to fetch book details by ID

--- src/main/java/com/booklend/backend/controllers/BookController.java
+++ src/main/java/com/booklend/backend/controllers/BookController.java
@@ -10,6 +10,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -44,10 +45,27 @@ public class BookController {
             params.get("image_url").toString() //  get URL of uploaded image
         );
         return ResponseEntity.ok(savedBook); // Return the saved book as response
-    } catch (Exception e) {
-        e.printStackTrace(); // Log the exception for debugging
-        return ResponseEntity.status(500).body(e.getMessage()); // Return error message with 500 status
+        } catch (Exception e) {
+            e.printStackTrace(); // Log the exception for debugging
+            return ResponseEntity.status(500).body(e.getMessage()); // Return error message with 500 status
+        }
     }
-}
+
+    // getting book by id
+    @GetMapping("/{bookId}")
+    public ResponseEntity<?> getBookById(@PathVariable Long bookId) { // get 
+        try {
+            // Call the service layer to fetch the book by ID
+            Book book = bookService.getBookById(bookId);
+            if (book == null) {
+               return ResponseEntity.status(404).body("Book not found"); // Message in response body
+            }
+            return ResponseEntity.ok(book);
+
+        } catch (Exception e) {
+            e.printStackTrace();
+            return ResponseEntity.status(500).body(e.getMessage());
+        }
+    }
 
 }
