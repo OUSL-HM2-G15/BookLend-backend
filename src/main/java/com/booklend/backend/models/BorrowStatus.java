a2e11eb836e0af36de246b49d1e36c2639c28259
Yathurahavi <s22010502@ousl.lk>
2026-01-11 21:47:37 +0530

    Feat/borrowed books/backend:  Backend implementation for user borrowed books flow (#22)
    Backend implementation for user borrowed books flow
    ---------
    
    Co-authored-by: zainab fathima <zainab9j3@gmail.com>

--- src/main/java/com/booklend/backend/models/BorrowStatus.java
+++ src/main/java/com/booklend/backend/models/BorrowStatus.java
@@ -1,8 +1,14 @@
 package com.booklend.backend.models;
 
+/**
+ * Enum for borrow request status
+ */
+
 public enum BorrowStatus {
     Pending,
     Accepted,
     Rejected,
-    Returned
+    Returned,
+    Cancelled // Cancelled requests if pending
+
 }
